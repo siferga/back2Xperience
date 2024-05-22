4fb30103e08341db9462f7d7455271a33c5610c4
Siferga <sifergadeveloper@gmail.com>
2024-05-22 14:00:15 +0200

    first commit

---
+++ src/main/java/com/siferga/back2Xperience/Back2XperienceApplication.java
@@ -0,0 +1,13 @@
+package com.siferga.back2Xperience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Back2XperienceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Back2XperienceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siferga/back2Xperience/Back2XperienceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siferga.back2Xperience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Back2XperienceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
